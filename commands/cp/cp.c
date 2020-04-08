677bb03e7b2154eb05fcc154b28041e2f938fd24
Yanis Chaabane <yanis.chaabane@epita.fr>
2020-04-08 10:34:21 +0200

    Trying to add some possibilities to the cp command

--- commands/cp/cp.c
+++ commands/cp/cp.c
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
 
 #define BUFF_SIZE 4096
 
@@ -36,15 +38,52 @@ int copy_file_to_file(int src, int dst)
     return EXIT_SUCCESS;
 }
 
-int copy_file_to_directory()
+int copy_file_to_directory(int src, char *source, char *dest)
 {
-
-    return EXIT_SUCCESS;
+    char path[BUFF_SIZE] = { 0 };
+    sprintf(path, "%s/%s", dest, source);
+    int dst = open(path, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    int ret = EXIT_SUCCESS;
+    if (copy_file_to_file(src, dst) < 0)
+        ret = EXIT_FAILURE;
+    close(dst);
+    return ret;
 }
 
-int copy_directory()
+int copy_directories(int src, char *source, char *dest)
 {
+    DIR *dir;
+    struct dirent *entry;
+    if (!(dir = opendir(source)))
+    {
+        printf("Error: can't open the file or directory : %s\n", source);
+        return EXIT_FAILURE;
+    }
+    if (copy_file_to_directory(src, source, dest) < 0)
+    {
+        close(src);
+        closedir(dir);
+        return EXIT_FAILURE;
+    }
+    char path[BUFF_SIZE] = { 0 };
+    sprintf(path, "%s/%s", dest, source);
+    while ((entry = readdir(dir)) != NULL)
+    {
+        char new_source[BUFF_SIZE] = { 0 };
+        sprintf(new_source, "%s/%s", source, entry->d_name);
+        int new_src = open(new_source, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 
+        if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+        {
+            copy_directories(new_src, new_source, path);
+        }
+        else
+        {
+            copy_file_to_directory(new_src, entry->d_name, path);
+        }
+        close(new_src);
+    }
+    closedir(dir);
     return EXIT_SUCCESS;
 }
 
@@ -72,14 +111,14 @@ int cp(int argc, char **argv)
     int src = open(source, O_RDONLY);
     if (src < 0)
     {
-        write(STDOUT_FILENO, "Error with the source\n", 22);
+        write(STDOUT_FILENO, "Error: with the source\n", 23);
         return EXIT_FAILURE;
     }
     int dst = open(dest, O_WRONLY | O_TRUNC | O_CREAT, 0644);
     if (dst < 0)
     {
         close(src);
-        write(STDOUT_FILENO, "Error with the source\n", 22);
+        write(STDOUT_FILENO, "Error: with the destination\n", 28);
         return EXIT_FAILURE;
     }
 
@@ -88,12 +127,37 @@ int cp(int argc, char **argv)
     if (fstat(src, &src_stats) < 0 || fstat(dst, &dst_stats) < 0)
     {
         printf("Error with stats\n");
+        close(src);
+        close(dst);
         return EXIT_FAILURE;
     }
 
-    if (copy_file_to_file(src, dst) < 0)
-        return EXIT_FAILURE;
+    int d_src = S_ISDIR(src_stats.st_mode);
+    int d_dst = S_ISDIR(dst_stats.st_mode);
 
-    return EXIT_SUCCESS;
+    int ret = EXIT_SUCCESS;
+
+    if (!d_src && !d_dst)
+    {
+        if (copy_file_to_file(src, dst) < 0)
+            ret = EXIT_FAILURE;
+    }
+    else if (!d_src && d_dst)
+    {
+        copy_file_to_directory(src, source, dest);
+    }
+    /*else if (d_src && d_dst)
+    {
+        copy_directories(src, source, dest);
+    }*/ 
+    else
+    {
+        printf("Error: can't cp directory to file\n");
+        ret = EXIT_FAILURE;
+    }
+
+    close(src);
+    close(dst);
+    return ret;
 
 }
--- commands/cp/cp.h
+++ commands/cp/cp.h
@@ -1,7 +1,7 @@
 int copy_file_to_file(int src, int dst);
 
-int copy_file_to_directory();
+int copy_file_to_directory(int src, char *source, char *dest);
 
-int copy_directory_to_directory();
+int copy_directories(int src, char *source, char *dest);
 
 int cp(int argc, char **argv);
