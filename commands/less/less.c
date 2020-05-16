178ccf4f3210c71f72b2647a3c0f927780adc7f4
Saule <saule.pievaityte@gmail.com>
2020-05-16 16:20:47 +0200

    less command improved, missing errors handled.

--- commands/less/less.c
+++ commands/less/less.c
@@ -4,55 +4,42 @@
 #include <string.h>
 #include <fcntl.h> 
 #include <err.h>
+#include <sys/stat.h>
 #define BUFFERSIZE 512
 
-int rmv(char *argv[]){
-
-    int a;
-    a = unlink(argv[1]);
-   
-    if(a<0){
-        write(STDOUT_FILENO, "Error occured doing rm command\n", 31);
-        return -1;
-    }
-    return 0;
-}
 int less(int argc, char *argv[])
 {
-    if (argc < 2)
-    {
-        write(STDOUT_FILENO, "Pick a different item to less\n", 30);
-        return -1;
-    }
-
-    int filedesc = open(argv[1], O_RDONLY);
-    if(filedesc < 0)
-    {
-        printf("There is no %s here", argv[1]);
-        printf("\n");
-        return -1;
-    }
-    else
-    {
-        while(1)
-        {
-            char message[BUFFERSIZE] = { 0 };
-            int desc = read(filedesc, message, BUFFERSIZE);
-            if (desc <= 0)
-                break;
-            write(STDOUT_FILENO, message, desc);
-        }
+  	struct stat file_info; 
+    stat(argv[1], &file_info);
+	
+	if (argc < 2 || (S_ISDIR(file_info.st_mode))){ 
 		
-		if(strcmp(argv[1],"HurryingStudent") == 0){
-			
-			rmv(argv);
-		}
-
-        close(filedesc);
-    }
-
-
-    char newline[1] = {'\n'};
-    write(STDOUT_FILENO, newline, 1);
+        write(STDOUT_FILENO, "Pick an item to less.\n", 22);
+        return EXIT_FAILURE;
+		
+    }else if(access( argv[1], F_OK ) == -1){
+		ssize_t k = strlen(argv[1]);
+		write(STDOUT_FILENO, "There is no ", 12);
+		write(STDOUT_FILENO, argv[1], k);
+		write(STDOUT_FILENO, " here.\n", 7);
+		return EXIT_FAILURE;
+	}else{        
+		int filedesc = open(argv[1], O_RDONLY);
+		if(filedesc < 0){
+			write(STDOUT_FILENO, "Error opening a file for reading.\n", 34);
+			return EXIT_FAILURE;
+		}else{
+			char message[BUFFERSIZE] = { 0 };
+			int desc = read(filedesc, message, BUFFERSIZE);
+			if (desc < 0){
+				write(STDOUT_FILENO, "Error reading an item.\n", 23);
+				return EXIT_FAILURE;
+			}
+			write(STDOUT_FILENO, message, desc);
+			close(filedesc);
+		}	
+	}
+    
+    write(STDOUT_FILENO, "\n", 1);
     return 0;
 }
