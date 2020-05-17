dff6b160a81273e780bfaeb5143f91de7de6163e
Saule <saule.pievaityte@gmail.com>
2020-05-17 13:36:39 +0200

    Fixing rm and ln error handling.

--- commands/rm/rm.c
+++ commands/rm/rm.c
@@ -7,68 +7,44 @@
 
 #define BUFF_SIZE 512
 
-
-char** parse_in(char *buf)
-{
-    char **parse = calloc(8, sizeof(char *));
-    if (parse == NULL)
-        errx(EXIT_FAILURE, "Error trying to allocate memory for the parse_input");
-
-    int args = 0;
-    char *separator = " ";
-    char *parsed;
-
-    parsed = strtok(buf, separator);
-    while (parsed != NULL)
-    {
-        parse[args] = parsed;
-        args++;
-        parsed = strtok(NULL, separator);
-    }
-    parse[args] = NULL;
-    return parse;
-
-}
-
-int perform_rm(char *argv[]){
+int perform_rm(char *argv){
 
     struct stat file_info; //pointer to stat struct
-    stat(argv[1], &file_info); //stat system call
+    stat(argv, &file_info); //stat system call
     int a; // for indicating if rm command was success
 
     if (S_ISDIR(file_info.st_mode)) { // is a directory
-        a = rmdir(argv[1]);
+        a = rmdir(argv);
     }else{                            // is a file
-        a = unlink(argv[1]);
+        a = unlink(argv);
     }
 
     if(a<0){
         write(STDOUT_FILENO, "Error occured doing rm command\n", 31);
-        return -1;
+        return EXIT_FAILURE;;
     }
     return 0;
 }
-int rm(char *argv[]){
-
-	if( access( argv[1], F_OK ) != -1 ) {
-		
-		char tmp[256];
-		char *pwd = getcwd(tmp, sizeof(tmp));
-		struct stat file_stat;
-		stat(pwd, &file_stat);
-		if( (file_stat.st_mode & S_IWUSR) &&
-			(file_stat.st_mode & S_IWGRP) &&
-			(file_stat.st_mode & S_IWOTH) ){
-				perform_rm(argv);
-			}else{
-				write(STDOUT_FILENO, "Rm doesn't work on this.\n",25);
-			}
-		
-		   
+int rm(int argc, char *argv[]){
 	
-	} else {
-		write(STDOUT_FILENO, "Wrong item name given.\n",23);
-	}
+	char tmp[256];
+	char *pwd = getcwd(tmp, sizeof(tmp));
+	struct stat file_stat;
+	stat(pwd, &file_stat);
+	if( (file_stat.st_mode & S_IWUSR) &&
+		(file_stat.st_mode & S_IWGRP) &&
+		(file_stat.st_mode & S_IWOTH) ){
+		for(int i=1; i < argc; i++){
+			if( access( argv[i], F_OK ) != -1 ) {
+				perform_rm(argv[i]);
+			}else {
+				write(STDOUT_FILENO, "Wrong item name given.\n",23);
+				return EXIT_FAILURE;
+			}
+		}
+	}else{
+		write(STDOUT_FILENO, "Rm doesn't work on this.\n",25);
+		return EXIT_FAILURE;
+		}
 	return 0;
-
 }
