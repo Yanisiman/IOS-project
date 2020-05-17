1a803f2cfefc6fcf661f3c7f8a35814ff7456a1e
MIkel <mikeliturria1@gmail.com>
2020-05-17 02:51:10 +0200

    Refactored makedir

--- commands/makedir/makedir.c
+++ commands/makedir/makedir.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -20,25 +20,41 @@ int makedir(int argc, char **argv){
     {
         write(STDOUT_FILENO, "Error: Insert the name of the directory\n", 40);
     }
-    else if(argc==2)
-    {
-
-        int fd= mkdir(argv[1], 0777);
-
-
-        if (fd ==-1) 
-        { 
-            write(STDOUT_FILENO, "Error: The directory already exists\n", 36);
-        }
-        if (fd ==0) {
-            write(STDOUT_FILENO, "Directory created successfully\n", 31);
-        }
-
-
-    }
     else
-    {
-        write(STDOUT_FILENO, "Error: Too many arguments\n", 26);
+    {   
+        int j=1;
+        int leer=0;
+        mode_t mode=0777;
+        if(argv[1][0] == '-'){
+            j=2;
+            if (strcmp(argv[1], "-v") == 0){
+                    leer=1;
+                }
+            if (strcmp(argv[1], "-m") == 0){
+                mode = strtol(argv[2], 0, 8);
+                mkdir(argv[3], mode);
+                return 0;
+                }
+        }    
+
+
+        for(int i=j;i<argc;i++){
+            int fd= mkdir(argv[i], 0777);
+            if (leer==1 && fd ==-1) 
+            {    
+                write(STDOUT_FILENO, "Error: The directory already exists: ", 37);
+                write(STDOUT_FILENO, argv[i], strlen(argv[i]));
+                write(STDOUT_FILENO, "\n", 1);
+            }
+           if (leer==1 && fd ==0) {
+                write(STDOUT_FILENO, "Directory created successfully: ", 32);
+                write(STDOUT_FILENO, argv[i], strlen(argv[i]));
+                write(STDOUT_FILENO, "\n", 1);
+        }
+    
+        }
+
+
     }
     return 0;
 }
